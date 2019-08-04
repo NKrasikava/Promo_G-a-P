36160dae33a245d87cf815c0ad33265e209bebf1
Natallia Krasikava <kraska17@gmail.com>
2019-08-04 19:39:19 +0300

    sleep problem

--- loader.js
+++ loader.js
@@ -1,6 +1,9 @@
-$(document).ready(function() {
+var canvas; // глобальная переменная
+var step = 5;
+
+$(document).ready(function() { //обработчик события загрузки страницы
     var elem = document.getElementById('loader'); //обратились к элементу по id в html 
-    var canvas = elem.getContext('2d'); //обозначили, что контекст у нас будет двумерный (рисуем двумерную графику)
+    canvas = elem.getContext('2d'); //обозначили, что контекст у нас будет двумерный (рисуем двумерную графику)
 
     canvas.lineWidth = 12; //толщина линии окружности
     canvas.lineCap = "round"; // скругления окончания линии
@@ -24,66 +27,30 @@ $(document).ready(function() {
     canvas.fillStyle = "white";
     canvas.fillText("75%", 255, 265);
 
+    drawProgress(75, 90);
 });
 
-// $(document).ready(function() {
-//     fillProgressBar('loader', '#f66', 80, 'Fija 1');
-//     newProgressBar('myCanvas');
-//     fillProgressBar('myCanvas', '#7c7', 50, 'Dinámica 1');
-//     newProgressBar('myCanvas2');
-//     fillProgressBar('myCanvas2', '#3375aa', 10, 'Dinámica 2');
-// });
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
+        }
+    }
+}
 
-// function newProgressBar(id) {
-//     var newProgress = document.createElement('div');
-//     $(newProgress).addClass("progress");
-//     var newContent = document.createElement('div');
-//     $(newContent).addClass("content");
-//     $(newContent).attr("id", id + "_div");
 
-//     var newCanvas =
-//         $('<canvas/>', {
-//             'id': id
-//         });
-//     newCanvas.attr("width", 250);
-//     newCanvas.attr("height", 250);
-
-//     $(newProgress).append(newContent);
-//     $(newProgress).append(newCanvas);
-//     $('.container').append(newProgress);
-// }
-
-// function fillProgressBar(id, color, progress, text) {
-//     var p = 0;
-//     var interval = setInterval(function() {
-
-//         var canvas = document.getElementById(id);
-//         var context = canvas.getContext('2d');
-//         var x = canvas.width / 2;
-//         var y = canvas.height / 2;
-//         var radius = 100;
-//         var startAngle = 1.5 * Math.PI;
-//         var endAngle = 1.5 * Math.PI + (2 * Math.PI) * p / 100;
-//         var counterClockwise = false;
-
-//         if (progress == null)
-//             progress = 100;
-
-//         context.clearRect(0, 0, 250, 250);
-
-//         context.beginPath();
-//         context.arc(x, y, radius, startAngle, endAngle, counterClockwise);
-//         context.lineWidth = 20;
-//         context.strokeStyle = color;
-//         context.stroke();
-
-//         if (text == null)
-//             text = "HOLA";
-
-//         $('#' + id + '_div').text(text);
-
-//         if (p == progress)
-//             clearInterval(interval);
-//         p++;
-//     }, 10);
-// }
+function drawProgress(go, end) {
+    console.log("drawProgress");
+    while (go <= end) {
+        sleep(2000);
+        var goDegree = (360 / 100) * go - 90;
+        var next = (360 / 100) * (go + step) - 90;
+        canvas.beginPath();
+        canvas.arc(250, 250, 70, goDegree * Math.PI / 180, next * Math.PI / 180, false); // координаты центра, радиус, начальный угол и конечный, высчитанные по формуле перевода градусов в радианы (y*PI/180)
+        console.log("goDegree: ", goDegree, " next: ", next);
+        canvas.stroke();
+        console.log("go: ", go, " next: ", go + step);
+        go = go + step;
+    }
+}
